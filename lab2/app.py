a91592c5c45771ff834e9079261223908d1fe885
getmubarak <getmub@gmail.com>
2020-07-15 12:44:59 +0530

    Update app.py

--- lab2/app.py
+++ lab2/app.py
@@ -1,37 +1,24 @@
 from flask import Flask
-from flask import request
 from pymongo import MongoClient
 from bson.json_util import dumps
 import json
 
-client = MongoClient("mongodb://mymongo_1:27017/")
-db = client.ContactDB
-
 app = Flask(__name__)
+client = MongoClient("ip address:27017/")
+db = client.TodoDB
 
-@app.route("/add_contact", methods = ['POST'])
-def add_contact():
-    try:
-        data = json.loads(request.data)
-        user_name = data['name']
-        user_contact = data['contact']
-        if user_name and user_contact:
-            status = db.Contacts.insert_one({
-                "name" : user_name,
-                "contact" : user_contact
-            })
-        return dumps({'message' : 'SUCCESS'})
-    except Exception as e:
-        return dumps({'error' : str(e)})
-
-@app.route("/get_all_contact", methods = ['GET'])
-def get_all_contact():
-    try:
-        contacts = db.Contacts.find()
-        return dumps(contacts)
-    except Exception as e:
-        return dumps({'error' : str(e)})
-
+@app.route("/add", methods = ['POST'])
+def add_contact() :
+    data = json.loads(request.data)
+    desc = data['desc']
+    if desc :
+        status = db.Todo.insert_one({"desc" : desc})
+        return dumps({ 'message' : 'SUCCESS' })
+        
+@app.route("/get", methods = ['GET'])
+def get_all_contact() :
+    todos = db.Todo.find()
+    return dumps(todos)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug = True, host = '0.0.0.0')
