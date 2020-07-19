6ac580fdee604ad501c440f6894a63bfb13827a1
getmubarak <getmub@gmail.com>
2020-07-19 10:01:10 +0530

    Update server.js

--- lab1/server.js
+++ lab1/server.js
@@ -8,11 +8,15 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
+app.get('/date', (req, res) => {
+        console.log('entered get date');
         var datetime = new Date();
+        
         var waitTill = new Date(new Date().getTime() + 5000);
         while(waitTill > new Date()){}
+        
         res.send(datetime.toISOString());
+        console.log('completed get date');
 });
 
 app.listen(PORT, HOST);
